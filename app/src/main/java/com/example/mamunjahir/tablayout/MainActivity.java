1b8f1d8017a658e8619fe61ce1f3e2f7d3d8869e
Mamunjahir <mamunjahir.com@gmail.com>
2017-05-10 13:46:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mamunjahir/tablayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mamunjahir.tablayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+    }
+}
